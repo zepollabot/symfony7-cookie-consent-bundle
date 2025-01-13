a83db11e9231ad73e61a7deefecbd9aab02a4bc3
chanond buranabunpot <chanond.b@app-alive.com>
2025-01-13 18:01:43 +0700

    fix config

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,16 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of the ConnectHolland CookieConsentBundle package.
- * (c) Connect Holland.
- */
-
 namespace Chanondb\CookieConsentBundle\DependencyInjection;
 
-use Chanondb\CookieConsentBundle\Enum\CategoryEnum;
-use Chanondb\CookieConsentBundle\Enum\DisabledRoutesEnum;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -20,35 +11,22 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('cookie_consent');
 
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $rootNode = /* @scrutinizer ignore-deprecated */ $treeBuilder->root('cookie_consent');
-        }
-
+        $rootNode = $treeBuilder->getRootNode();
         $rootNode
             ->children()
-                ->variableNode('categories')
-                    ->defaultValue([CategoryEnum::CATEGORY_ANALYTICS, CategoryEnum::CATEGORY_MARKETING, CategoryEnum::CATEGORY_PREFERENCES])
+                ->arrayNode('categories')
+                    ->scalarPrototype()->end()
+                    ->defaultValue(['necessary', 'preferences', 'analytics'])
                 ->end()
-                ->booleanNode('use_logger')
-                    ->defaultTrue()
+                ->booleanNode('use_logger')->defaultTrue()->end()
+                ->booleanNode('http_only')->defaultTrue()->end()
+                ->scalarNode('form_action')->defaultNull()->end()
+                ->booleanNode('csrf_protection')->defaultTrue()->end()
+                ->arrayNode('disabled_routes')
+                    ->scalarPrototype()->end()
+                    ->defaultValue(['privacy', 'imprint'])
                 ->end()
-                ->booleanNode('http_only')
-                    ->defaultTrue()
-                ->end()
-                ->scalarNode('form_action')
-                    ->defaultNull()
-                ->end()
-                ->booleanNode('csrf_protection')
-                    ->defaultTrue()
-                ->end()
-                ->variableNode('disabled_routes')
-                    ->defaultValue([DisabledRoutesEnum::DISABLED_ROUTE_PRIVACY, DisabledRoutesEnum::DISABLED_ROUTE_IMPRINT])
-                ->end()
-            ->end()
-        ;
+            ->end();
 
         return $treeBuilder;
     }
