d3bcb4035283009f063d1cabb91ceb18845e9f4e
chanond buranabunpot <chanond.b@app-alive.com>
2025-01-13 12:45:14 +0700

    fix extension

--- DependencyInjection/CBCookieConsentExtension.php
+++ DependencyInjection/CBCookieConsentExtension.php
@@ -7,16 +7,15 @@ namespace Chanondb\CookieConsentBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\DependencyInjection\Attribute\AsExtension;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-#[AsExtension(name: 'cb_cookie_consent')]
 class CBCookieConsentExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
 
+        // Process the configuration and set parameters
         $config = $this->processConfiguration($configuration, $configs);
 
         $container->setParameter('cb_cookie_consent.categories', $config['categories']);
@@ -26,9 +25,11 @@ class CBCookieConsentExtension extends Extension
         $container->setParameter('cb_cookie_consent.csrf_protection', $config['csrf_protection']);
         $container->setParameter('cb_cookie_consent.disabled_routes', $config['disabled_routes']);
 
+        // Load service definitions
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
     }
+
     public function getAlias(): string
     {
         return 'cb_cookie_consent';
